36e744951dca31fef2fa6ef021d12d584e8e20dc
Skia <skia@libskia.so>
2016-04-21 03:18:48 +0200

    Small fix in rc.lua

--- .config/awesome/my_widgets.lua
+++ .config/awesome/my_widgets.lua
@@ -11,7 +11,7 @@ cpuwidget:set_background_color("#494B4F")
 cpuwidget:set_border_color(nil)
 cpuwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 0, 10 }, stops = { {0, "#FF0000" }, { 0.5, "#88A175" }, { 1, "#00FF00" }} })
 vicious.cache(vicious.widgets.cpu)
-vicious.register(cpuwidget, vicious.widgets.cpu, "$2")
+vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
 -- Mem usage
 memwidget = awful.widget.progressbar()
 memwidget:set_width(8)
